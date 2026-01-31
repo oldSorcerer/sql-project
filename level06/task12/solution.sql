f9dd470a08ee9d9a76cbc4d26a6e7b94821554d9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2026-01-31 13:44:00 +0300

    sql level 06 task 12

---
+++ level06/task12/solution.sql
@@ -0,0 +1,3 @@
+-- Используем LEFT JOIN для объединения таблиц students и enrollments
+-- Проверяем, где course_name из таблицы enrollments равен NULL
+-- Подсчитываем количество таких студентов с помощью COUNT
